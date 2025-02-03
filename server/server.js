298578685df06239e94aa93fab00b6f471651d07
ashoka <ashoklande73@gmail.com>
2025-02-03 15:27:16 +0530

    .

--- server/server.js
+++ server/server.js
@@ -10,10 +10,18 @@ import ConnectCloudinary from './config/cloudinary.js';
  
 
 const app=express();
-app.use(cors({
-    origin: "https://bike-portal-frontend.vercel.app",  // Specify your frontend origin
-     
+app.options('*', cors({
+    origin: "https://bike-portal-frontend.vercel.app",
+    methods: ["POST", "GET", "OPTIONS"],
+    credentials: true
 }));
+
+// Your routes go after this
+
+// app.use(cors({
+//     origin: "https://bike-portal-frontend.vercel.app",  // Specify your frontend origin
+     
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
