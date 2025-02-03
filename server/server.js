53467ea94c36af5185903144c1f6fce6b7d29c65
ashoka <ashoklande73@gmail.com>
2025-02-03 13:52:40 +0530

    Cors-added

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ import ConnectCloudinary from './config/cloudinary.js';
 
 const app=express();
 
-app.use(cors())
+app.use(cors(),{
+    origin:["https://bike-portal-backend.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+})
 app.use(express.json());
 app.use(cookieParser());
 await ConnectTodb();
